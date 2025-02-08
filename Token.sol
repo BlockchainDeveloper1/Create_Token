262927612fdcddf5028e7811224e08d2118dd2c5
Sanjana <sanjanagoutam062@gmail.com>
2025-02-08 23:31:27 +0530

    concept of smart contract

--- Token.sol
+++ Token.sol
@@ -1,10 +1,13 @@
 //SPDX-License-Identifier:MIT
 
+//solidity version used
 pragma solidity ^0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+//This imports the ERC-20 token standard from the openzeppelin library.
 
 contract MyCoin is ERC20{
+//This line defines a new contract named MyCoin that inherits from the ERC20 contract    
     constructor() ERC20("MY COIN" , "MC"){
      _mint(msg.sender, 10000 * 10**3);
 }
